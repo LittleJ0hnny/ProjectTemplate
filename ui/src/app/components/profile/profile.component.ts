efeef20096cdf4bf51405958ce5300df38015911
OKravtsiv <pro100_zi_lvov@ukr.net>
2019-06-18 01:59:52 +0300

    Continue working with profile form

--- ui/src/app/components/profile/profile.component.ts
+++ ui/src/app/components/profile/profile.component.ts
@@ -3,6 +3,8 @@ import {Component, HostListener, Inject, OnInit} from '@angular/core';
 import {DOCUMENT} from "@angular/common";
 import {ProfileData} from "./profileData";
 import {ButtonModule} from 'primeng/button';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputMaskModule} from 'primeng/inputmask';
 
 @Component({
   selector: 'app-profile',
@@ -27,6 +29,22 @@ export class ProfileComponent implements OnInit {
   lastNameControl: FormControl = new FormControl('', [
     Validators.maxLength(255)
   ]);
+  genderControl: FormControl = new FormControl('', [
+  ]);
+  birthDateControl: FormControl = new FormControl('', [
+  ]);
+  zipCodeControl: FormControl = new FormControl('', [
+  ]);
+  mobilePhoneControl: FormControl = new FormControl('', [
+  ]);
+
+  //Form controls visibility
+  firstName: boolean = false;
+  lastName: boolean = false;
+  gender: boolean = false;
+  birthDate: boolean = false;
+  zipCode: boolean = false;
+  mobilePhone: boolean = false;
 
   constructor(private formBuilder: FormBuilder,
               @Inject(DOCUMENT) private document: any) {
@@ -34,12 +52,20 @@ export class ProfileComponent implements OnInit {
     this.profileData = new ProfileData();
     this.profileData.firstName = 'Ostap';
     this.profileData.lastName = 'Kravtsiv';
+    this.profileData.gender = 'Male';
+    this.profileData.birthDate = new Date();
+    this.profileData.zipCode = 79005;
+    this.profileData.mobilePhone = '(063)-903-3115';
   }
 
   ngOnInit() {
     this.profileForm = this.formBuilder.group({
       firstName: this.firstNameControl,
-      lastName: this.lastNameControl
+      lastName: this.lastNameControl,
+      gender: this.genderControl,
+      birthDate: this.birthDateControl,
+      zipCode: this.zipCodeControl,
+      mobilePhone: this.mobilePhoneControl
     });
     this.imagePreview = 'https://previews.123rf.com/images/artemstepanov/artemstepanov1606/artemstepanov160601228/57881761-vector-male-face-avatar-template-pictogram-button-round-trendy-flat-icon-with-man-for-business-inter.jpg';
   }
@@ -56,6 +82,35 @@ export class ProfileComponent implements OnInit {
       this.imagePreview = reader.result;
     };
     reader.readAsDataURL(this.uploadedFile);
+  }
+
+  changeVisibility(fieldName: string) {
+    switch(fieldName) {
+      case 'firstName': {
+        this.firstName = !this.firstName;
+        break;
+      }
+      case 'lastName': {
+        this.lastName = !this.lastName;
+        break;
+      }
+      case 'gender': {
+        this.gender = !this.gender;
+        break;
+      }
+      case 'birthDate': {
+        this.birthDate = !this.birthDate;
+        break;
+      }
+      case 'zipCode': {
+        this.zipCode = !this.zipCode;
+        break;
+      }
+      case 'mobilePhone': {
+        this.mobilePhone = !this.mobilePhone;
+        break;
+      }
+    }
   }
 
   toggleEdit(data: string) {
--- ui/src/app/modules/prime-ng/prime-ng.module.ts
+++ ui/src/app/modules/prime-ng/prime-ng.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import {ButtonModule} from "primeng/button";
 import {PanelModule} from 'primeng/panel';
 import {FileUploadModule} from "primeng/primeng";
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {CalendarModule} from 'primeng/calendar';
+import {InputMaskModule} from 'primeng/inputmask';
 
 @NgModule({
   exports: [
     ButtonModule,
     FileUploadModule,
-    PanelModule
+    PanelModule,
+    RadioButtonModule,
+    CalendarModule,
+    InputMaskModule
   ],
   declarations: []
 })
